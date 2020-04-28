b57a932d2752a01e9f81767dc89e620edbb44a91
Delano Junior <delano.duarte@gmail.com>
2020-04-28 23:18:05 +0100

    changed the storage to disk storage

--- config/storage.js
+++ config/storage.js
@@ -1,7 +1,14 @@
 const multer = require("multer")
 
+const disk_storage = multer.diskStorage({
+    dest: 'uploads/',
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
 const storage = multer({
-    dest: 'uploads/'
+    storage: disk_storage
 })
 
 module.exports = {
